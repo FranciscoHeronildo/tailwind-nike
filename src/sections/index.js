66595355f3fa4c9d3114032682fc4cc3d98f2f9c
FranciscoHeronildo <heronildo.2010@hotmail.com>
2023-11-20 14:00:38 -0300

    init project nike

---
+++ src/sections/index.js
@@ -0,0 +1,10 @@
+import CustomerReviews from './CustomerReviews';
+import Hero from './Hero';
+import Footer from './Footer';
+import PopularProducts from './PopularProducts';
+import Services from './Services';
+import SpecialOffers from './SpecialOffers';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+
+export { CustomerReviews, Hero, Footer, SpecialOffers, PopularProducts, Services, Subscribe, SuperQuality }
